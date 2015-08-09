697688581a5112a44ab50995de75c962b91b0f53
Viswanadh <nadhbv@yahoo.com>
2015-08-09 21:30:08 +0100

    plot2.R

---
+++ plot2.R
@@ -0,0 +1,23 @@
+plot2<-function(){
+  tf<-read.csv2('household_power_consumption.txt') 
+  tf1<-subset(tf,Date=='1/2/2007')
+  tf2<-subset(tf,Date=='2/2/2007')
+  tf1$Time<-as.character(tf1$Time)
+  tf2$Time<-as.character(tf2$Time)
+  tf1$Time<-as.POSIXct(tf1$Time,format='%H:%M:%S')
+  tf2$Time<-as.POSIXct(tf2$Time,format='%H:%M:%S')+86400
+  tff<-rbind(tf1,tf2)
+  #tff$Date<-as.character(tff$Date)
+  #tff$Time<-as.character(tff$Time)
+  tff$Global_active_power<-as.character(tff$Global_active_power)
+  tff$Global_reactive_power<-as.character(tff$Global_reactive_power)
+  tff$Voltage<-as.character(tff$Voltage)
+  tff$Global_intensity<-as.character(tff$Global_intensity)
+  tff$Sub_metering_1<-as.character(tff$Sub_metering_1)
+  tff$Sub_metering_2<-as.character(tff$Sub_metering_2)
+  tff$Sub_metering_3<-as.character(tff$Sub_metering_3)
+  
+  plot(tff$Time,tff$Global_active_power,type='l',xlab='',ylab='Global Active Power (kilowatts)')  
+  dev.copy(png,file='plot2.png')
+  dev.off()
+}
